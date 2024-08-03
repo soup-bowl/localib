44d2b65d1cf97ab9dbfc28bc7ff7d3c48ba25392
soup-bowl <code@soupbowl.io>
2024-08-03 21:57:01 +0100

    Fixed search, stupid mistake.

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonSearchbar, IonTitle, IonToolbar } from "@ionic/react"
+import { IonAvatar, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonSearchbar, IonText, IonTitle, IonToolbar } from "@ionic/react"
 import { useQuery } from "@tanstack/react-query"
 import { IReleases, getCollectionReleases } from "../api"
 import { FullpageLoading } from "../components"
@@ -6,29 +6,27 @@ import { useEffect, useState } from "react"
 
 const SearchPage: React.FC = () => {
 	const [filterData, setFilterData] = useState<IReleases[]>([])
-	// const { data, isLoading } = useQuery<IReleases[]>({
-	// 	queryKey: ["collection"],
-	// 	queryFn: () => getCollectionReleases(),
-	// 	staleTime: 1000 * 60 * 60 * 24, // 24 hours
-	// })
+	const { data, isLoading } = useQuery<IReleases[]>({
+		queryKey: ["collection"],
+		queryFn: () => getCollectionReleases(),
+		staleTime: 1000 * 60 * 60 * 24, // 24 hours
+	})
 
-	// const searchData = (data: IReleases[], search: string) => {
-	// 	const lowerCaseSearchTerm = search.toLowerCase();
-	// 	setFilterData(data.filter(item =>
-	// 		item.basic_information.title.toLowerCase().includes(lowerCaseSearchTerm) ||
-	// 		item.basic_information.artists.some(artist => artist.name.toLowerCase().includes(lowerCaseSearchTerm))
-	// 	));
-	// }
+	const searchData = (data: IReleases[], search: string) => {
+		const lowerCaseSearchTerm = search.toLowerCase();
+		setFilterData(data.filter(item =>
+			item.basic_information.title.toLowerCase().includes(lowerCaseSearchTerm) ||
+			item.basic_information.artists.some(artist => artist.name.toLowerCase().includes(lowerCaseSearchTerm))
+		));
+	}
 
-	useEffect(() => console.log("Filtered data", filterData), [filterData])
-
-	// if (isLoading) {
-	// 	return (
-	// 		<IonPage>
-	// 			<FullpageLoading />
-	// 		</IonPage>
-	// 	)
-	// }
+	if (isLoading) {
+		return (
+			<IonPage>
+				<FullpageLoading />
+			</IonPage>
+		)
+	}
 
 	return (
 		<IonPage>
@@ -37,27 +35,26 @@ const SearchPage: React.FC = () => {
 					<IonTitle>Search</IonTitle>
 				</IonToolbar>
 				<IonToolbar>
-					{/* <IonSearchbar debounce={1000} onIonInput={(ev) => searchData(data ?? [], ev.target.value?.toLowerCase() ?? '')} /> */}
+					<IonSearchbar debounce={1000} onIonInput={(ev) => searchData(data ?? [], ev.target.value?.toLowerCase() ?? '')} />
 				</IonToolbar>
 			</IonHeader>
-			{/* {filterData.length > 0 && */}
-				<IonList>
-					<IonItem>Test 1</IonItem>
-					<IonItem>Test 2</IonItem>
-					<IonItem>Test 3</IonItem>
-					<IonItem>Test 4</IonItem>
-					<IonItem>Test 5</IonItem>
-					<IonItem>Test 6</IonItem>
-					<IonItem>Test 7</IonItem>
-					<IonItem>Test 8</IonItem>
-					{/* {filterData.map((album, index) => (
+			<IonContent fullscreen>
+			{filterData.length > 0 &&
+				<IonList lines="full">
+					{filterData.map((album, index) => (
 						<IonItem key={index}>
-							{album.basic_information.title}
+							<IonAvatar aria-hidden="true" slot="start">
+								<img alt="" src={album.basic_information.thumb} />
+							</IonAvatar>
+							<IonLabel>
+								<strong>{album.basic_information.title}</strong><br/>
+								<IonText>{album.basic_information.artists[0].name}</IonText>
+							</IonLabel>
 						</IonItem>
-					))} */}
+					))}
 				</IonList>
-			{/* } */}
-			<IonContent fullscreen></IonContent>
+			}
+			</IonContent>
 		</IonPage>
 	)
 }
