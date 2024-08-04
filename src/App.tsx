b53dd2f3d9a45bd52567a2211048fc86d79e01a3
soup-bowl <code@soupbowl.io>
2024-08-04 20:14:13 +0100

    Redundant include.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Redirect, Route } from "react-router-dom"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { QueryClient } from "@tanstack/react-query"
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client"
 import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister"
 import {
