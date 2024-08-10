8887d7b3fd54ecd08c0325452745a2b284f37327
soup-bowl <code@soupbowl.io>
2024-08-10 23:28:01 +0100

    Linter suggestions.

---
+++ backend/Discapp.API/Models/AvailableRecord.cs
@@ -0,0 +1,8 @@
+namespace Discapp.API.Models
+{
+    public class AvailableRecord
+    {
+        public int RecordID { get; set; }
+        public string Image { get; set; } = "";
+    }
+}
--- backend/Discapp.API/Models/RecordReply.cs
+++ backend/Discapp.API/Models/RecordReply.cs
@@ -8,10 +8,4 @@ namespace Discapp.API.Models
         public List<AvailableRecord> Available { get; set; } = new List<AvailableRecord>();
         public List<int> Queued { get; set; } = new List<int>();
     }
-
-    public class AvailableRecord
-    {
-        public int RecordID { get; set; }
-        public string Image { get; set; } = "";
-    }
 }
