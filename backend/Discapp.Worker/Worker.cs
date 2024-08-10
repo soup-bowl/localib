cf28d137120f8328f5132a509b6d72d4cebc0bd3
soup-bowl <code@soupbowl.io>
2024-08-10 20:57:30 +0100

    Check and requeue expired items.

--- backend/Discapp.Worker/Worker.cs
+++ backend/Discapp.Worker/Worker.cs
@@ -39,54 +39,105 @@ public class Worker : BackgroundService
 
                 ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-                Queue? queueItem = await dbContext.Queue.FirstOrDefaultAsync(stoppingToken);
+                await FilterOldRecords(dbContext, stoppingToken);
 
-                if (queueItem != null)
+                await ProcessQueue(dbContext, stoppingToken);
+            }
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+
+    private async Task ProcessQueue(ApplicationDbContext dbContext, CancellationToken stoppingToken)
+    {
+        Queue? queueItem = await dbContext.Queue.FirstOrDefaultAsync(stoppingToken);
+
+        if (queueItem != null)
+        {
+            try
+            {
+                HttpResponseMessage response = await GetApiResponseWithRetryAsync($"releases/{queueItem.RecordID.ToString()}", 3, 1000, stoppingToken);
+                response.EnsureSuccessStatusCode();
+
+                DiscogsRelease? releaseData = await response.Content.ReadFromJsonAsync<DiscogsRelease>();
+
+                if (releaseData?.Thumb != null)
                 {
-                    try
+                    string imageUrl = releaseData.Thumb;
+                    byte[] imageBytes = await GetApiResponseWithRetryAsync(imageUrl, 3, 1000, stoppingToken).Result.Content.ReadAsByteArrayAsync(stoppingToken);
+
+                    string filePath = Path.Combine(_pathOptions.ImagePath, queueItem.RecordID.ToString() + ".jpg");
+                    await File.WriteAllBytesAsync(filePath, imageBytes, stoppingToken);
+
+                    Record? existingRecord = await dbContext.Records
+                        .FirstOrDefaultAsync(r => r.RecordID == queueItem.RecordID, stoppingToken);
+
+                    if (existingRecord != null)
                     {
-                        HttpResponseMessage response = await GetApiResponseWithRetryAsync($"releases/{queueItem.RecordID}", 3, 1000, stoppingToken);
-                        response.EnsureSuccessStatusCode();
-
-                        DiscogsRelease? releaseData = await response.Content.ReadFromJsonAsync<DiscogsRelease>();
-
-                        if (releaseData?.Thumb != null)
-                        {
-                            string imageUrl = releaseData.Thumb;
-                            byte[] imageBytes = await GetApiResponseWithRetryAsync(imageUrl, 3, 1000, stoppingToken).Result.Content.ReadAsByteArrayAsync(stoppingToken);
-
-                            string filePath = Path.Combine(_pathOptions.ImagePath, queueItem.RecordID.ToString() + ".jpg");
-                            await File.WriteAllBytesAsync(filePath, imageBytes, stoppingToken);
-
-                            Record newRecord = new()
-                            {
-                                RecordID = queueItem.RecordID,
-                                FilePath = $"{queueItem.RecordID}.jpg",
-                                Recorded = DateTime.Now
-                            };
-
-                            dbContext.Records.Add(newRecord);
-                            dbContext.Queue.Remove(queueItem);
-                            await dbContext.SaveChangesAsync(stoppingToken);
-
-                            _logger.LogInformation($"Processed and removed Queue item with ID {queueItem.Id}, saved image to {filePath}");
-                        }
-                        else
-                        {
-                            _logger.LogWarning($"No thumbnail found for release ID {queueItem.RecordID}");
-                        }
+                        existingRecord.FilePath = $"{queueItem.RecordID.ToString()}.jpg";
+                        existingRecord.Recorded = DateTime.Now;
+                        dbContext.Records.Update(existingRecord);
                     }
-                    catch (Exception ex)
+                    else
                     {
-                        _logger.LogError(ex, $"An error occurred while processing the queue ID {queueItem.Id}.");
+                        Record newRecord = new()
+                        {
+                            RecordID = queueItem.RecordID,
+                            FilePath = $"{queueItem.RecordID.ToString()}.jpg",
+                            Recorded = DateTime.Now
+                        };
+                        dbContext.Records.Add(newRecord);
                     }
+
+                    dbContext.Queue.Remove(queueItem);
+                    await dbContext.SaveChangesAsync(stoppingToken);
+
+                    _logger.LogInformation($"Processed and removed Queue item with ID {queueItem.Id}, saved image to {filePath}");
                 }
                 else
                 {
-                    _logger.LogInformation("Nothing in the queue.");
+                    _logger.LogWarning($"No thumbnail found for release ID {queueItem.RecordID}");
+                    dbContext.Queue.Remove(queueItem);
+                    await dbContext.SaveChangesAsync(stoppingToken);
                 }
             }
-            await Task.Delay(1000, stoppingToken);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An error occurred while processing the queue ID {queueItem.Id}.");
+                dbContext.Queue.Remove(queueItem);
+                await dbContext.SaveChangesAsync(stoppingToken);
+            }
+        }
+        else
+        {
+            _logger.LogInformation("Nothing in the queue.");
+        }
+    }
+
+    private async Task FilterOldRecords(ApplicationDbContext dbContext, CancellationToken stoppingToken)
+    {
+        DateTime decay = DateTime.Now.AddMonths(-3);
+        List<Record> oldRecords = await dbContext.Records
+            .Where(r => r.Recorded < decay)
+            .ToListAsync(stoppingToken);
+
+        if (oldRecords.Any())
+        {
+            foreach (Record oldRecord in oldRecords)
+            {
+                bool isAlreadyInQueue = await dbContext.Queue
+                    .AnyAsync(q => q.RecordID == oldRecord.RecordID, stoppingToken);
+
+                if (!isAlreadyInQueue)
+                {
+                    Queue newQueueItem = new()
+                    {
+                        RecordID = oldRecord.RecordID
+                    };
+                    dbContext.Queue.Add(newQueueItem);
+                }
+            }
+            await dbContext.SaveChangesAsync(stoppingToken);
+            _logger.LogInformation($"Checked {oldRecords.Count} old records and added them to the queue if not already present.");
         }
     }
 
