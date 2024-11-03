0f0f391a4c0cf7ec6a9b48e193a9977e5e705c6b
soup-bowl <code@soupbowl.io>
2024-11-03 10:56:30 +0000

    Adjusted album display mode.

--- frontend/src/modal/ViewAlbumDetails.tsx
+++ frontend/src/modal/ViewAlbumDetails.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react"
+import { useState } from "react"
 import {
 	IonModal,
 	IonHeader,
@@ -23,6 +23,7 @@ import { getReleaseInfo, IRelease, IReleases } from "@/api"
 import { useAuth } from "@/hooks"
 
 enum TabSet {
+	Info,
 	Tracks,
 	Credits,
 	Notes,
@@ -37,17 +38,13 @@ interface DisplayProps {
 
 const ViewAlbumDetails: React.FC<DisplayProps> = ({ album, open, onClose }) => {
 	const [{ token }] = useAuth()
-	const [tabState, setTabState] = useState<number>(1)
+	const [tabState, setTabState] = useState<number>(0)
 
 	const { data, isSuccess } = useQuery<IRelease>({
 		queryKey: ["release", album.id],
 		queryFn: () => getReleaseInfo(token ?? "", album.id),
 		staleTime: Infinity,
 	})
-
-	if (isSuccess) {
-		console.log(album, data)
-	}
 
 	return (
 		<IonModal isOpen={open}>
@@ -80,150 +77,157 @@ const ViewAlbumDetails: React.FC<DisplayProps> = ({ album, open, onClose }) => {
 				</IonToolbar>
 			</IonHeader>
 			<IonContent className="ion-padding">
-				<div style={{ display: "flex", justifyContent: "center" }}>
+				<div style={{ display: "flex", justifyContent: "center", marginBottom: 15 }}>
 					<img src={album.image_base64 ? album.image_base64 : album.basic_information.thumb} alt="" />
 				</div>
-				<IonList inset>
-					<IonItem>
-						<IonLabel>
-							<h2>Title</h2>
-							<p>{album.basic_information.title}</p>
-						</IonLabel>
-					</IonItem>
-					<IonItem>
-						<IonLabel>
-							<h2>Artist</h2>
-							<p>{album.basic_information.artists.map((artist) => artist.name).join(", ")}</p>
-						</IonLabel>
-					</IonItem>
-					<IonItem>
-						<IonLabel>
-							<h2>Genre</h2>
-							<p>{album.basic_information.genres.map((genre) => genre).join(", ")}</p>
-						</IonLabel>
-					</IonItem>
-					<IonItem>
-						<IonLabel>
-							<h2>Label</h2>
-							<p>{album.basic_information.labels.map((label) => label.name).join(", ")}</p>
-						</IonLabel>
-					</IonItem>
-					<IonItem>
-						<IonLabel>
-							<h2>Styles</h2>
-							<p>{album.basic_information.styles.map((style) => style).join(", ")}</p>
-						</IonLabel>
-					</IonItem>
-					<IonItem>
-						<IonLabel>
-							<h2>Owned Formats</h2>
-							<p>
-								{album.basic_information.formats
-									.map((format) => `${format.name} (${format.qty})`)
-									.join(", ")}
-							</p>
-						</IonLabel>
-					</IonItem>
-					<IonItem>
-						<IonLabel>
-							<h2>Barcode</h2>
-							<p>{album.barcode ?? ""}&nbsp;</p>
-						</IonLabel>
-					</IonItem>
-				</IonList>
-				{isSuccess && (
+				<IonSegment
+					value={tabState}
+					onIonChange={(e: CustomEvent<SegmentChangeEventDetail>) => {
+						// @ts-expect-error aaa
+						setTabState(parseInt(e.detail.value ?? "0"))
+					}}
+					disabled={!isSuccess}
+				>
+					<IonSegmentButton value={TabSet.Info}>
+						<IonLabel>Info</IonLabel>
+					</IonSegmentButton>
+					<IonSegmentButton value={TabSet.Tracks}>
+						<IonLabel>Tracks</IonLabel>
+					</IonSegmentButton>
+					<IonSegmentButton value={TabSet.Credits}>
+						<IonLabel>Credits</IonLabel>
+					</IonSegmentButton>
+					<IonSegmentButton value={TabSet.Notes}>
+						<IonLabel>Notes</IonLabel>
+					</IonSegmentButton>
+					<IonSegmentButton value={TabSet.IDs}>
+						<IonLabel>IDs</IonLabel>
+					</IonSegmentButton>
+				</IonSegment>
+				{tabState === TabSet.Info && (
 					<>
 						<IonList inset>
 							<IonItem>
 								<IonLabel>
-									<h2>Released</h2>
+									<h2>Title</h2>
+									<p>{album.basic_information.title}</p>
+								</IonLabel>
+							</IonItem>
+							<IonItem>
+								<IonLabel>
+									<h2>Artist</h2>
+									<p>{album.basic_information.artists.map((artist) => artist.name).join(", ")}</p>
+								</IonLabel>
+							</IonItem>
+							<IonItem>
+								<IonLabel>
+									<h2>Genre</h2>
+									<p>{album.basic_information.genres.map((genre) => genre).join(", ")}</p>
+								</IonLabel>
+							</IonItem>
+							<IonItem>
+								<IonLabel>
+									<h2>Label</h2>
+									<p>{album.basic_information.labels.map((label) => label.name).join(", ")}</p>
+								</IonLabel>
+							</IonItem>
+							<IonItem>
+								<IonLabel>
+									<h2>Styles</h2>
+									<p>{album.basic_information.styles.map((style) => style).join(", ")}</p>
+								</IonLabel>
+							</IonItem>
+							<IonItem>
+								<IonLabel>
+									<h2>Owned Formats</h2>
 									<p>
-										{data.released_formatted} {data.country}
+										{album.basic_information.formats
+											.map((format) => `${format.name} (${format.qty})`)
+											.join(", ")}
 									</p>
 								</IonLabel>
 							</IonItem>
 							<IonItem>
 								<IonLabel>
-									<h2>Community</h2>
-									<p>
-										{data.community.have.toLocaleString()} have,{" "}
-										{data.community.want.toLocaleString()} want
-									</p>
+									<h2>Barcode</h2>
+									<p>{album.barcode ?? ""}&nbsp;</p>
 								</IonLabel>
 							</IonItem>
 						</IonList>
-						<IonSegment
-							value={tabState}
-							onIonChange={(e: CustomEvent<SegmentChangeEventDetail>) => {
-								// @ts-expect-error aaa
-								setTabState(parseInt(e.detail.value ?? "0"))
-							}}
-						>
-							<IonSegmentButton value={TabSet.Tracks}>
-								<IonLabel>Tracks</IonLabel>
-							</IonSegmentButton>
-							<IonSegmentButton value={TabSet.Credits}>
-								<IonLabel>Credits</IonLabel>
-							</IonSegmentButton>
-							<IonSegmentButton value={TabSet.Notes}>
-								<IonLabel>Notes</IonLabel>
-							</IonSegmentButton>
-							<IonSegmentButton value={TabSet.IDs}>
-								<IonLabel>IDs</IonLabel>
-							</IonSegmentButton>
-						</IonSegment>
-						{tabState === TabSet.Tracks && (
-							<IonList inset>
-								{data.tracklist.map((track, index) => (
-									<IonItem key={index}>
-										<IonNote slot="start">{track.position}</IonNote>
-										<IonLabel>{track.title}</IonLabel>
-										<IonNote slot="end">{track.duration}</IonNote>
-									</IonItem>
-								))}
-							</IonList>
-						)}
-						{tabState === TabSet.Credits && (
-							<IonList inset>
-								{data.extraartists.map((artist, index) => (
-									<IonItem key={index}>
-										<IonLabel>
-											<h2>{artist.name}</h2>
-											<p>{artist.role}</p>
-										</IonLabel>
-									</IonItem>
-								))}
-							</IonList>
-						)}
-						{tabState === TabSet.Notes && (
+						{isSuccess && (
 							<IonList inset>
 								<IonItem>
 									<IonLabel>
-										{data.notes.split("\n").map((line) => (
-											<>
-												{line}
-												<br />
-											</>
-										))}
+										<h2>Released</h2>
+										<p>
+											{data.released_formatted} {data.country}
+										</p>
+									</IonLabel>
+								</IonItem>
+								<IonItem>
+									<IonLabel>
+										<h2>Community</h2>
+										<p>
+											{data.community.have.toLocaleString()} have,{" "}
+											{data.community.want.toLocaleString()} want
+										</p>
 									</IonLabel>
 								</IonItem>
 							</IonList>
 						)}
-						{tabState === TabSet.IDs && (
-							<IonList inset>
-								{data.identifiers.map((id, index) => (
-									<IonItem key={index}>
-										<IonLabel>
-											<h2>
-												{id.type} {id.description}
-											</h2>
-											<p>{id.value}</p>
-										</IonLabel>
-									</IonItem>
-								))}
-							</IonList>
-						)}
 					</>
+				)}
+
+				{tabState === TabSet.Tracks && (
+					<IonList inset>
+						{data!.tracklist.map((track, index) => (
+							<IonItem key={index}>
+								<IonNote slot="start">{track.position}</IonNote>
+								<IonLabel>{track.title}</IonLabel>
+								<IonNote slot="end">{track.duration}</IonNote>
+							</IonItem>
+						))}
+					</IonList>
+				)}
+				{tabState === TabSet.Credits && (
+					<IonList inset>
+						{data!.extraartists.map((artist, index) => (
+							<IonItem key={index}>
+								<IonLabel>
+									<h2>{artist.name}</h2>
+									<p>{artist.role}</p>
+								</IonLabel>
+							</IonItem>
+						))}
+					</IonList>
+				)}
+				{tabState === TabSet.Notes && (
+					<IonList inset>
+						<IonItem>
+							<IonLabel>
+								{data!.notes.split("\n").map((line) => (
+									<>
+										{line}
+										<br />
+									</>
+								))}
+							</IonLabel>
+						</IonItem>
+					</IonList>
+				)}
+				{tabState === TabSet.IDs && (
+					<IonList inset>
+						{data!.identifiers.map((id, index) => (
+							<IonItem key={index}>
+								<IonLabel>
+									<h2>
+										{id.type} {id.description}
+									</h2>
+									<p>{id.value}</p>
+								</IonLabel>
+							</IonItem>
+						))}
+					</IonList>
 				)}
 			</IonContent>
 		</IonModal>
