1c7b1821694b7fd20b546e9bea1a980dc94dbab9
soup-bowl <code@soupbowl.io>
2025-02-17 21:51:48 +0000

    Harmonised app port.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,10 @@
 #! /bin/env bash
 if [ ! -z ${GITPOD_HOST+x} ]; then
-	FE_URL="https://8100-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}"
+	FE_URL="https://5000-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}"
 elif [ ! -z ${CODESPACE_NAME+x} ]; then
-	FE_URL="https://${CODESPACE_NAME}-8100.app.github.dev"
+	FE_URL="https://${CODESPACE_NAME}-5000.app.github.dev"
 else
-	FE_URL="http://localhost:8100"
+	FE_URL="http://localhost:5000"
 fi
 
 git update-index --assume-unchanged .devcontainer/devcontainer.env
